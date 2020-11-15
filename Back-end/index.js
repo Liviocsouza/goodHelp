55e41724f9ccb26bfbcb6b42fe8a3cca11f8d855
Liviocsouza <liviocavalcantid@gmail.com>
2020-11-14 21:27:13 -0300

    init

---
+++ Back-end/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/',(req, res)=> {
+    res.send("Agora foi em")
+})
+
+const rotas = require('./rotas')
+app.use('/api', rotas)
+
+
+const port = 3001
+
+app.listen(port, () => {
+    console.log("Server rodando em http://localhost:",port)
+
+})
